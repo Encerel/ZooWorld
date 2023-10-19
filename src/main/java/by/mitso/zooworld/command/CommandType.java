43da47a715f38ccfaffef675a3a3af3c195187a6
Andrei Yankavets <andreyyankovez@gmail.com>
2023-10-19 14:45:50 +0300

    [feature] Add created commands to 'CommandType' class

--- src/main/java/by/mitso/zooworld/command/CommandType.java
+++ src/main/java/by/mitso/zooworld/command/CommandType.java
@@ -1,6 +1,6 @@
 package by.mitso.zooworld.command;
 
-import by.mitso.zooworld.command.impl.UnknownCommand;
+import by.mitso.zooworld.command.impl.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +11,16 @@ public class CommandType {
 
     static {
         commands.put("unknown_command", new UnknownCommand());
+        commands.put("log_in", new LogInCommand());
+        commands.put("to_main", new ToMainCommand());
+        commands.put("to_sign_in", new ToSignInCommand());
+        commands.put("to_personal_page", new ToPersonalPageCommand());
+        commands.put("to_sign_up", new ToSignUpCommand());
+        commands.put("sign_up", new SignUpCommand());
+        commands.put("log_out", new LogOutCommand());
+        commands.put("to_cart_page", new ToCartPageCommand());
+        commands.put("to_products_page", new ToProductsPageCommand());
+        commands.put("add_product_to_cart", new AddProductToCartCommand());
     }
 
 
