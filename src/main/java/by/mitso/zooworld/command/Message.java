057a731cf8cff7c8d80bfc1e983741cd796737bb
Andrei Yankavets <andreyyankovez@gmail.com>
2023-10-19 13:21:32 +0300

    [feature] Add 'Message' class with message constants

---
+++ src/main/java/by/mitso/zooworld/command/Message.java
@@ -0,0 +1,25 @@
+package by.mitso.zooworld.command;
+
+public class Message {
+
+    public static final String INCORRECT_EMAIL_OR_LOGIN="Incorrect email or password";
+    public static final String SUCCESSFUL ="Successful";
+    public static final String UNSUCCESSFUL ="Unsuccessful";
+    public static final String USER_ALREADY_EXISTS ="User with that login already exists";
+    public static final String USER_CREATED ="User successfully created";
+    public static final Object PASSWORDS_MISMATCH = "Password and confirmed password do not match";
+    public static final String WRONG_PASSWORD_FORMAT = "Wrong password format (Password should contain at least one letter and one symbol)";
+    public static final String WRONG_EMAIL_FORMAT = "Wrong email format";
+    public static final String WRONG_FIRST_OR_LAST_NAME_FORMAT = "Wrong first or last name  format";
+    public static final String WRONG_USER_DATA = "Wrong user data";
+    public static final String NO_USER_WITH_ID = "No user with id = ";
+    public static final String NO_USERS_WITH_FIRST_NAME = "No users with first name = ";
+    public static final String NO_USERS_WITH_LAST_NAME = "No users with last name = ";
+    public static final String NO_REGISTERED_USER_WITH_EMAIL = "No registered user with email = ";
+    public static final String WRONG_PASSWORD = "Wrong password";
+    public static final String WRONG_PHONE_NUMBER_FORMAT = "Wrong phone number format";
+    public static final Object NOTHING_HERE = "Nothing here";
+
+
+    private Message() {}
+}
