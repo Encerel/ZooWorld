f486de768f0fe17e7b4e01f186efdcf62edb7c26
Andrei Yankavets <andreyyankovez@gmail.com>
2023-10-19 14:36:08 +0300

    [feature] Create command for transer on singup and signin pages

---
+++ src/main/java/by/mitso/zooworld/command/impl/ToSignInCommand.java
@@ -0,0 +1,20 @@
+package by.mitso.zooworld.command.impl;
+
+import by.mitso.zooworld.command.Command;
+import by.mitso.zooworld.command.PagePath;
+import by.mitso.zooworld.command.ParameterAndAttribute;
+import by.mitso.zooworld.command.Router;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+public class ToSignInCommand implements Command {
+
+    @Override
+    public Router execute(HttpServletRequest request) {
+        Router router = new Router();
+        HttpSession session = request.getSession();
+        session.setAttribute(ParameterAndAttribute.CURRENT_PAGE, PagePath.TO_SIGN_IN_PAGE);
+        router.setPagePath(PagePath.SIGN_IN);
+        return router;
+    }
+}
